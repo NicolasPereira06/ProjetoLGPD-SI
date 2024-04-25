be8d05286ff4e73cefed6d094d72de33f36e62c4
Eliézer Lopes <eli74350@gmail.com>
2024-04-24 22:40:51 -0300

    Dados do Usuário

---
+++ client/src/pages/UserScreen/UserScreen.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import '../UserScreen/userscreen.css';
+
+const UserScreen: React.FC = () => {
+  return (
+    <div className='container'>
+      <div className='main'>
+        <div className="user-screen">
+          <div className="title-edit-container">
+            <h1 className='title'>Meus dados</h1>
+            <DropdownButton id="dropdown-basic-button" title="Opções" variant="primary">
+              <Dropdown.Item href="#/action-1">Editar dados</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Mudar senha</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Excluir dados</Dropdown.Item>
+            </DropdownButton>
+          </div>
+          <div className="table-container">
+            <table>
+              <tbody>
+                <tr>
+                  <th>Primeiro Nome</th>
+                  <td>Usuário 1</td>
+                </tr>
+                <tr>
+                  <th>Último Nome</th>
+                  <td>Usuário 2</td>
+                </tr>
+                <tr>
+                  <th>CPF</th>
+                  <td>123.456.789-00</td>
+                </tr>
+                <tr>
+                  <th>Data de Nascimento</th>
+                  <td>01/01/1990</td>
+                </tr>
+                <tr>
+                  <th>Logradouro</th>
+                  <td>Rua Exemplo</td>
+                </tr>
+                <tr>
+                  <th>Número do Logradouro</th>
+                  <td>123</td>
+                </tr>
+                <tr>
+                  <th>Bairro</th>
+                  <td>Bairro Exemplo</td>
+                </tr>
+                <tr>
+                  <th>CEP</th>
+                  <td>12345-678</td>
+                </tr>
+                <tr>
+                  <th>Cidade</th>
+                  <td>Cidade Exemplo</td>
+                </tr>
+                <tr>
+                  <th>Estado</th>
+                  <td>Estado Exemplo</td>
+                </tr>
+                <tr>
+                  <th>Email</th>
+                  <td>usuario1@example.com</td>
+                </tr>
+                <tr>
+                  <th>Senha</th>
+                  <td>********</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserScreen;
--- client/src/routes/Rotas.tsx
+++ client/src/routes/Rotas.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import UserScreen from "../pages/UserScreen/UserScreen"
 
 export const Rotas = () => {
     return (
@@ -9,6 +10,7 @@ export const Rotas = () => {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/userscreen" element={<UserScreen />} />
                 <Route path="/" element={<Navigate to={'/login'} />} />
                 <Route path="*" element={<h1>PÁGINA NÃO ENCONTRADA</h1>}/>
             </Routes>
